2ffe5874ebefb54c7a50f2f896042092f5b2f86b
Rajiv Shah <rshah@pobox.com>
2023-08-14 13:46:55 -0500

    wandb update

--- config/train_simpsons_char.py
+++ config/train_simpsons_char.py
@@ -10,8 +10,8 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt'
+wandb_project = 'simpsons-char'
+wandb_run_name = 'nano-gpt'
 
 dataset = 'simpsons'
 gradient_accumulation_steps = 1
